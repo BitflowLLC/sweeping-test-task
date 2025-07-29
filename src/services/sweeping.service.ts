2c89c9fbcfc6ed1a76633ed75f45669ad0521fe7
Aamir Husain <cgw0034@gmail.com>
2025-07-29 15:49:14 +0530

    Sweeping Task is completed 9 passed, 9 total

--- src/services/sweeping.service.ts
+++ src/services/sweeping.service.ts
@@ -1,4 +1,4 @@
-import { SimulatedWalletService, WalletId } from './wallet.service';
+import { NotEnoughGasError, NotEnoughTokenError, SimulatedWalletService, WalletId } from './wallet.service';
 
 export interface SweepingService {
   /**
@@ -11,12 +11,41 @@ export interface SweepingService {
 }
 
 export class TaskSweepingService implements SweepingService {
+
   constructor(
     private walletService: SimulatedWalletService,
     private mainWalletId: string,
-  ) {}
+  ) { }
 
   async sweepAll(walletIds: WalletId[], toWalletId: WalletId): Promise<void> {
     // TODO: implement sweeping algorithm
+    const GAS_FEE = this.walletService['gasFee']; // e.g., 0.01
+    for (const walletId of walletIds) {
+      const ethBalance = this.walletService.getBalance(walletId, 'ETH');
+      const usdtBalance = this.walletService.getBalance(walletId, 'USDT');
+      // Skip if USDT is zero
+      if (usdtBalance <= 0) { continue; }
+
+      try {        
+        if (ethBalance < GAS_FEE) { // not enough ETH
+          
+          const topUpAmount = GAS_FEE - ethBalance;
+          const mainEth = this.walletService.getBalance(toWalletId, 'ETH');
+          
+          if (mainEth < topUpAmount) { continue; }
+          this.walletService.send(toWalletId, walletId, 'ETH', topUpAmount);
+        }
+        // Now sweep USDT only
+        this.walletService.send(walletId, toWalletId, 'USDT', usdtBalance);
+      } catch (error) {
+        if (error instanceof NotEnoughGasError) {
+          continue;
+        } else if(error instanceof NotEnoughTokenError) {
+          continue;
+        } else {
+          throw error;
+        }
+      }
+    }
   }
 }
